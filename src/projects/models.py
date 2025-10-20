70b331787524f0ab59f328420038b9b84145e67c
diogopinto <diogopinto@oversharp.org>
2025-10-20 20:02:34 +0100

    - Estrutura de dados para os projetos.

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# ----------------------------------------------
+# projects/models.py
+# Models for the projects app
+# 
+# <diogopinto> 2025+
+# ----------------------------------------------
 
-# Create your models here.
+from django.db import models
+from users.models import CustomUser
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    # EN: related_name solves the reverse accessor clash issue
+    members = models.ManyToManyField(CustomUser, related_name="members")
